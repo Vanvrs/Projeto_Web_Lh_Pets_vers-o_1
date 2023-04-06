a0456d88e0d8b1341d447c0a4e350dcd577a0df6
Vanvrs <107437663+Vanvrs@users.noreply.github.com>
2023-04-06 20:31:29 -0300

    Criação de rotas

---
+++ Clientes.cs
@@ -0,0 +1,16 @@
+//dotnet new web --use-program-main carrega os pacotes necessários para rodar
+
+namespace Projeto_Web_Lh_Pets_versão_1
+{
+    class Clientes
+    {
+        public string? cpf_cnpj {get; set;}
+        public string nome {get; set;}
+        public string endereco {get; set;}
+        public string rg_ie {get; set;}
+        public string tipo {get; set;}
+        public float valor {get; set;}
+        public float valor_imposto {get; set;}
+        public float total {get; set;}
+    }
+}
